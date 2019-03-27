7d0a84a7dc11730d7954b66d533e354fceff7b47
jinxueqiang <jinxueqiang@nobook.com>
2019-03-27 16:50:22 +0800

    fix: 修改签名规则

--- src/server.js
+++ src/server.js
@@ -2,22 +2,24 @@ import * as md5
   from 'blueimp-md5';
 /** ******************************************************
  *      注意: 此文件为模仿后台处理,一下方法与数据都必须放到后台,
- *      即 appkey 必须要隐藏, 即签名sign为后端输出
+ *      即 appSecret 必须要隐藏, 即签名sign为后端输出
  ******************************************************* */
 export const SECRET_DATA = {
-  appid: 'xxxx', // nobook提供
-  appkey: 'xxxx' // 重要注意: nobook提供(appkey需后台保存,如果前台暴露引发任何损失概不负责)
+  appKey: 'xxx', // nobook提供
+  appSecret: 'xxx' // 重要注意: nobook提供(appSecret需后台保存,如果前台暴露引发任何损失概不负责)
 };
 
 /**
- * 生成签名, 规则为: md5(appid appkey nickname pid timestamp uid)
- * @param uid
+ * 生成签名, 规则为: md5(appKey appSecret nickname pidScope timestamp uniqueId usertype)
+ * usertype 为常量 0
+ * pidScope 如果要为多个产品授权,则多个产品用逗号隔开
+ * @param uniqueId
  * @param nickname
  * @returns {*}
  */
-export function getServerData(uid, nickname, pid) {
+export function getServerData(uniqueId, nickname, pidScope) {
   const timestamp = new Date().getTime().toString().substring(0, 10);
-  const sign = md5(SECRET_DATA.appid + SECRET_DATA.appkey + nickname + pid + timestamp + uid);
+  const sign = md5(SECRET_DATA.appKey + SECRET_DATA.appSecret + nickname + pidScope + timestamp + uniqueId + '0');
   return {
     timestamp,
     sign
